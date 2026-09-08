4655089ddcc87b5453f08e43d951ae00f0bba921
Raj <raaj18322@gmail.com>
2026-09-08 20:01:07 +0545

    Create python6.py

---
+++ Stage 1/Python/python6.py
@@ -0,0 +1,15 @@
+#string methods
+
+course = "   Python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find("pro"))
+print(course.find("Pro"))
+print(course.replace("p","j"))
+
+print("pro" in course)
+print("java" not in course)
