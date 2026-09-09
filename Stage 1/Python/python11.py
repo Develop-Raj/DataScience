d5de5efabf594b830e5c050a304cfe44548cf958
Raj <raaj18322@gmail.com>
2026-09-09 11:47:09 +0545

    Add temperature condition statement in python11.py

---
+++ Stage 1/Python/python11.py
@@ -0,0 +1,11 @@
+#condition statement
+
+temp = 35;
+if temp > 30:
+    print("it's warm")
+    print("drink water")
+elif temp > 20:
+    print("it's nice")
+else: 
+    print("its cold")
+print("Done");
