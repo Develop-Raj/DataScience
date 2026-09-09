c0bde07289ffc847125d04860d5625f752e512cd
Raj <raaj18322@gmail.com>
2026-09-09 12:08:02 +0545

    Add greeting functions in python22.py

---
+++ Stage 1/Python/python22.py
@@ -0,0 +1,13 @@
+#types of function
+
+def greet(name ):
+    print(f"hi {name}")
+
+def get_greeting(name):
+    return f"HI {name}"
+
+message = get_greeting("Raj Developer")
+
+greet("raj DEv")
+
+print(message)
