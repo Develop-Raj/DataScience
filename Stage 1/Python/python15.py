e049fd05afdf042db0904572647b1ea3fcf5479d
Raj <raaj18322@gmail.com>
2026-09-09 11:49:48 +0545

    Implement for loops in python15.py
    Added multiple for loops demonstrating iteration and printing.

---
+++ Stage 1/Python/python15.py
@@ -0,0 +1,14 @@
+# for loop
+
+for i in range(10):
+    print("hello")
+
+for i in range(3):
+    print("Attempt", i, (i+1)*".")
+
+
+for i in range(1, 10):
+    print("Attempt", i, (i+1)*".")
+
+for i in range(1, 10, 2):
+    print("Attempt", i, (i+1)*".")
