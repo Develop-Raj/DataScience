472eb51f09ada0da532dc48f7bfba08f14cb8ad6
Raj <raaj18322@gmail.com>
2026-09-09 11:53:21 +0545

    Create python18.py

---
+++ Stage 1/Python/python18.py
@@ -0,0 +1,13 @@
+#iterable
+for x in range(5):
+    print(x);
+for x in "Python":
+    print(x)
+
+str1 = "Raj Developer"
+for x in str1:
+    print(x)
+
+list1 = [1,2,3,4,5]
+for x in list1:
+    print(x);
