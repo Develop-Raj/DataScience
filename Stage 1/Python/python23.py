a0a60f00c946c0ef1778bde2c3db85ccacbffb94
Raj <raaj18322@gmail.com>
2026-09-10 09:44:13 +0545

    Create python23.py

---
+++ Stage 1/Python/python23.py
@@ -0,0 +1,15 @@
+#list - mutable, duplicates, ordered, heterogenous
+a = []
+b = [12,12,13,14,15,16,17,True, "Raj"]
+
+print(b[0])
+print(b[0:5])
+print(b[-2])
+
+
+c = [12,12,13,14,15,16,17]
+for i in range(len(c)):
+    print(c[i])
+
+for item in c:
+    print(item)
